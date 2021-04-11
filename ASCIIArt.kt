2bcd203b0ce43a39b5d08bca2eefb322750d8a2c
Luiz Alexandre Ferreira Martinez <alxmartinez@gmail.com>
2021-04-11 11:00:50 -0300

    Sending the .kt file.

---
+++ ASCIIArt.kt
@@ -0,0 +1,36 @@
+
+// Write your code below
+fun main() {
+
+    println("     /---------------\\        ")
+    println("    /                 \\       ")
+    println("   /                   \\      ")
+    println("  |---------------------|      ")
+    println("  |                     |      ")
+    println("  |    |----|    |----| |      ")
+    println("  |    |____|    |    | |      ")
+    println("  |              |    | |      ")
+    println("  |______________|____|_|_     ")
+
+}
+
+/*
+println("     /--------------\          ")
+println("    /                 \        ")
+println("   /                    \      ")
+println("  |---------------------|      ")
+println("  |                     |      ")
+println("  |    |----|    |----| |      ")
+println("  |    |____|    |    | |      ")
+println("  |              |    | |      ")
+println("  |______________|____|_|_     ")
+
+*/
+
+
+
+
+
+
+
+
